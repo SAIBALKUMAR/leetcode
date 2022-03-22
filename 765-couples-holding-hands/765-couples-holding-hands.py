2cd336ff271ef98b2c1f50ba3f4379344269d06d
Saibal Kumar <kumarsaibal6@gmail.com>
2022-03-22 19:43:17 +0530

    Time: 97 ms (6.35%), Space: 13.2 MB (92.06%) - LeetHub

---
+++ 765-couples-holding-hands/765-couples-holding-hands.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def minSwapsCouples(self, row):
+        """
+        :type row: List[int]
+        :rtype: int
+        """
+        dix={}
+        for i in range(len(row)):
+            dix[row[i]]=i
+        i=0
+        count=0
+        while i<len(row)-1:
+            if row[i]%2==0:
+                if row[i+1]==row[i]+1:
+                    i+=2
+                else:
+                    temp=dix[row[i]+1]
+                    row[i+1],row[dix[row[i]+1]]=row[dix[row[i]+1]],row[i+1]
+                    dix[row[i+1]]=i+1
+                    dix[row[temp]]=temp
+                    i+=2
+                    count+=1
+                    print(row)
+            else:
+                if row[i+1]==row[i]-1:
+                    i+=2
+                else:
+                    temp=dix[row[i]-1]
+                    row[i+1],row[dix[row[i]-1]]=row[dix[row[i]-1]],row[i+1]
+                    dix[row[i+1]]=i+1
+                    dix[row[temp]]=temp
+                    i+=2
+                    count+=1
+                    print(row)
+        return count       
+        
+                    
