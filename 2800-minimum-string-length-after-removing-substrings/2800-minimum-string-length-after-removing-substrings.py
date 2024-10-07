1a6e0ae7ada20ecf71bde02fde9dc63b7e08a8fd
Saibal Kumar <kumarsaibal6@gmail.com>
2024-10-07 19:47:52 +0530

    Time: 47 ms (39.58%), Space: 16.6 MB (41.77%) - LeetHub

---
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.py
@@ -0,0 +1,10 @@
+from copy import deepcopy
+class Solution:
+    def minLength(self, s: str) -> int:
+        while ("AB" in s or "CD" in s):
+            s = s.replace("AB", "")
+            s = s.replace("CD", "")
+
+        return len(s)
+        
+                
