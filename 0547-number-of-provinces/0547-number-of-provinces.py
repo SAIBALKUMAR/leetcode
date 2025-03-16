fdbd05992630c91532866163e31d908b52b492ae
Saibal Kumar <kumarsaibal6@gmail.com>
2025-03-16 16:44:02 +0530

    Time: 15 ms (17.87%), Space: 19 MB (57.33%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,22 +1,29 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n = len(isConnected)
-        parent = list(range(n))
+        parent = [i for i in range(n)]
+        rank=[1]*n
+        ans=n
 
-        def find(x):
-            while x != parent[x]:
-                parent[x] = parent[parent[x]]
-                x = parent[x]
-            return x
+        def find(node):
+            while node!=parent[node]:
+               parent[node]=parent[parent[node]]
+               node=parent[node]
+            return node
 
-        def union(x, y):
-            x = find(x)
-            y = find(y)
-            parent[x] = y
-
+        def union(n1,n2):
+            p1,p2 = find(n1),find(n2)
+            if p1==p2:
+                return 0
+            if rank[p1]>rank[p2]:
+                parent[p2]=p1
+                rank[p1]+=rank[p2]
+            else:
+                parent[p1]=p2
+                rank[p2]+=rank[p1]
+            return 1
         for i in range(n):
             for j in range(n):
-                if isConnected[i][j]:
-                    union(i, j)
-        
-        return sum(parent[x] == x for x in range(n))
+                if isConnected[i][j]==1:
+                    ans-=union(i,j)
+        return ans
