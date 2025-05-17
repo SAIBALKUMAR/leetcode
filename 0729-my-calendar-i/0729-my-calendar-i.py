a93dc4a34354dce864278f244bcfb6aae65f6d48
Saibal Kumar <kumarsaibal6@gmail.com>
2025-05-18 00:42:16 +0530

    Time: 35 ms (86.66%), Space: 18.8 MB (46.69%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -1,13 +1,35 @@
+class TreeNode:
+    def __init__(self, start,end):
+        self.left = None
+        self.right = None
+        self.start = start
+        self.end = end
+
+    def insert(self, start,end):
+        curr = self
+        while True:
+            if curr.start >= end:
+                if not curr.left:
+                    curr.left = TreeNode(start,end)
+                    return True
+                curr = curr.left
+            elif curr.end <= start:
+                if not curr.right:
+                    curr.right = TreeNode(start,end)
+                    return True
+                curr = curr.right
+            else:
+                return False
+
 class MyCalendar:
 
     def __init__(self):
-        self.intervals = []
+        self.root = None
     def book(self, startTime: int, endTime: int) -> bool:
-        for s, e in self.intervals:
-            if e > startTime and endTime > s:
-                return False
-        self.intervals.append((startTime,endTime))
-        return True
+        if not self.root:
+            self.root = TreeNode(startTime, endTime)
+            return True
+        return self.root.insert(startTime,endTime)
 
 
 # Your MyCalendar object will be instantiated and called as such:
