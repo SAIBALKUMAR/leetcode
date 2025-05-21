ecd184d40e9c65ce7a70ad10f63f0aa456163839
Saibal Kumar <kumarsaibal6@gmail.com>
2025-05-21 07:24:32 +0530

    Time: 96 ms (80.5%), Space: 23.4 MB (49.74%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -2,13 +2,11 @@ class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
         resLength = 0
         hashmapSum = {0:-1}
-        prevSum = 0
-        nums = [-1 if num == 0 else 1 for num in nums]
+        requiredSum = 0
         for index, num in enumerate(nums):
-            requiredSum = prevSum + num
+            requiredSum += 1 if num == 1 else -1
             if requiredSum in hashmapSum:
                 resLength = max(index - hashmapSum[requiredSum],resLength)
             else:
                 hashmapSum[requiredSum] = index
-            prevSum = requiredSum
         return resLength
